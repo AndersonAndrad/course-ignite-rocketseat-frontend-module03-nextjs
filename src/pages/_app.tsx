483729f909df4c200037e50ab2b2bdb8f70068ff
AndersonAndrad <anderson_andrade_@outlook.com>
2021-08-02 13:27:58 -0300

    style - create header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,18 @@
 import { AppProps } from "next/app"
 // styles
 import '../../styles/global.scss'
+// shared components
+import { Header } from '../components/Header'
 
 function MyApp ( { Component, pageProps }: AppProps ) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+
+//TODO: Pass all images to next/images
