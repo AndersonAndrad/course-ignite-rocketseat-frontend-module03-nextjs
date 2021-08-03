64e6776b07657e911827d76c9112bf2d213d0096
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-08-02 21:34:27 -0300

    styles - create subscribe button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@
 import Head from 'next/head'
 // styles
 import styles from '../../styles/home.module.scss'
+// shared components
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home () {
   return (
@@ -14,6 +16,7 @@ export default function Home () {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publication <br /> <span>for $9.90 month</span></p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
